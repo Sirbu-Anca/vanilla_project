0513a822beebf80bd318246f4e1c91fb68979c72
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-01-28 18:16:55 +0200

    #6 login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/6 Login.

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'common.php';
+session_start();
+
+if (isset($_SESSION['username']) && $_SESSION['username'] == ADMIN_CREDENTIALS['USERNAME']) {
+    header('location:products.php');
+    die();
+}
+$inputData = [];
+$inputErrors = [];
+
+if (isset($_POST['submit'])) {
+    if ($_POST['username']) {
+        $inputData['username'] = strip_tags($_POST['username']);
+    } else {
+        $inputErrors['usernameError'] = 'Enter a username';
+    }
+    if ($_POST['password']) {
+        $inputData['password'] = strip_tags($_POST['password']);
+    } else {
+        $inputErrors['passwordError'] = 'Enter a password';
+    }
+    if (!count($inputErrors)) {
+        if ($inputData['username'] == ADMIN_CREDENTIALS['USERNAME'] && $inputData['password'] == ADMIN_CREDENTIALS['PASSWORD']) {
+            $_SESSION['username'] = $inputData['username'];
+            header('location:products.php');
+            die();
+        }
+        $inputErrors['failedMessage'] = 'Wrong username and password';
+    }
+}
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login page</title>
+</head>
+<body>
+<form action="login.php" method="post">
+    <input type="text" name="username" placeholder="Username"
+           value="<?php echo isset($inputData['username']) ? $inputData['username'] : ''; ?>">
+    <span><?php echo isset($inputErrors['usernameError']) ? $inputErrors['usernameError'] : ''; ?></span>
+    <br><br>
+    <input type="password" name="password" placeholder="Password"
+           value="<?php echo isset($inputData['password']) ? $inputData['password'] : ""; ?>">
+    <span><?php echo isset($inputErrors['passwordError']) ? $inputErrors['passwordError'] : ''; ?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Login">
+    <span><?php echo isset($inputErrors['failedMessage']) ? $inputErrors['failedMessage'] : ''; ?></span>
+</form>
+
+</body>
+</html>
