83dd64f6743264c27aaf5d7f8973af70f60bac46
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-01-29 15:27:49 +0200

    #3 Standard and procedures http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/3 Tranlate function added.

--- common.php
+++ common.php
@@ -4,11 +4,25 @@ require_once 'config.php';
 function getDbConnection()
 {
     try {
-        $connection = new PDO("mysql:host=".SERVER_NAME.";dbname=".DB_NAME."", USERNAME, PASSWORD);
+        $connection = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DB_NAME . "", USERNAME, PASSWORD);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
 
-    }catch (PDOException $exception) {
+    } catch (PDOException $exception) {
         echo 'Connection failed: ' . $exception->getMessage();
+    }
+}
+
+$translation = [];
+function translate($label)
+{
+    return isset($GLOBALS['translation'][$label]) ? $GLOBALS['translation'][$label] : $label;
+}
+
+function checkForAuthentication()
+{
+    if (!isset($_SESSION['username']) && $_SESSION['username'] != ADMIN_CREDENTIALS['USERNAME']) {
+        header('Location: login.php');
+        die();
     }
 }
