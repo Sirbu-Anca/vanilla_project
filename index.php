0250c6a18ab50de386cad5f341137d548795b33f
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-01-29 15:22:37 +0200

    #5 index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/1 Translate labels.

--- index.php
+++ index.php
@@ -23,32 +23,47 @@ $products = $stm->fetchAll(PDO::FETCH_OBJ);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
+    <title><?php echo translate('Products') ?></title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+
+        td {
+            text-align: left;
+        }
+
+        #bottom {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-<table border="1px solid black">
-    <tr>
-        <th>id</th>
-        <th>image</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th>Action</th>
-    </tr>
+
+<table>
     <?php foreach ($products as $product) { ?>
         <tr>
-            <td><?php echo $product->id ?></td>
-            <td><img src="" alt="image"></td>
-            <td><?php echo $product->title ?></td>
-            <td><?php echo $product->description ?></td>
-            <td><?php echo $product->price ?></td>
-            <td><a href="cart.php?add_id=<?php echo $product->id ?>">Add</a></td>
+            <td>
+                <img src="" alt="image">
+            </td>
+            <td>
+                <?php echo $product->title ?><br>
+                <?php echo $product->description ?><br>
+                <?php echo $product->price . ' ' . translate('eur') ?> <br><br>
+            </td>
+            <td>
+                <a href="cart.php?add_id=<?php echo $product->id ?>"><?php echo translate('Add') ?></a>
+            </td>
         </tr>
         <?php
     }
     ?>
+    <tr>
+        <td id="bottom" colspan="3">
+            <a href="cart.php"><?php echo translate('Go to cart') ?></a>
+        </td>
+    </tr>
 </table>
-<a href="cart.php">Go to cart</a>
 </body>
 </html>
 
