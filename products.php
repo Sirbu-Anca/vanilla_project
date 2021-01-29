acaf4fde977cd5b9b8dc02eced50c9602037fb8f
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-01-29 15:31:32 +0200

    #7 products.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/7 Delete product.

--- products.php
+++ products.php
@@ -1,16 +1,29 @@
 <?php
 require_once 'common.php';
 session_start();
-if (!isset($_SESSION['username'])) {
-    header('location:login.php');
+checkForAuthentication();
+$connection = getDbConnection();
+
+if (isset($_GET['logout'])) {
+    unset($_SESSION['username']);
+    header('Location: login.php');
     die();
 }
-$connection = getDbConnection();
+
+if (isset($_POST['deleteItem'])) {
+    $productToBeDeleted = $_POST['deleteItem'];
+    $sql = $connection->prepare('DELETE FROM products where id=:id');
+    $sql->bindParam(':id', $productToBeDeleted, PDO::PARAM_INT);
+    $sql->execute();
+    header('Location: products.php');
+    die();
+}
+
 $stm = $connection->prepare("SELECT * FROM products");
 $stm->execute();
 $products = $stm->fetchAll(PDO::FETCH_OBJ);
 $connection = null;
-unset($_SESSION['username']);
+
 ?>
 <html>
 <head>
@@ -18,30 +31,51 @@ unset($_SESSION['username']);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
+    <title><?php echo translate('Products') ?></title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+
+        td {
+            text-align: left;
+        }
+
+        #bottom {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-<table border="1px solid black">
-    <tr>
-        <th>Image</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th colspan="2">Action</th>
-    </tr>
+<table>
     <?php foreach ($products as $product) { ?>
         <tr>
-            <td><?php echo $product->image ?></td>
-            <td><?php echo $product->title ?></td>
-            <td><?php echo $product->description ?></td>
-            <td><?php echo $product->price ?></td>
+            <td>
+                <img src="" alt="image">
+            </td>
+            <td>
+                <?php echo $product->title ?><br>
+                <?php echo $product->description ?><br>
+                <?php echo $product->price . ' ' . translate('eur') ?><br>
+            </td>
             <td><a href="">Edit</a></td>
-            <td><a href="">Delete</a></td>
+            <td>
+                <form action="products.php" method="post"
+                ">
+                <input type="hidden" name="deleteItem" value="<?php echo $product->id; ?>">
+                <input type="submit" value="Delete">
+                </form>
+            </td>
         </tr>
         <?php
     }
     ?>
+    <tr>
+        <td id="bottom" colspan="4">
+            <a href="product.php"><?php echo translate('Add') ?></a>
+            <a href="products.php?logout=logout"><?php echo translate('Logout') ?></a>
+        </td>
+    </tr>
 </table>
-<a href="product.php">Add</a> &nbsp &nbsp <a href="products.php">Logout</a>
 </body>
 </html>
