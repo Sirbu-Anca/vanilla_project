a2c3ac1e144c6425e6b216883765e61032ebd317
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-02-02 17:59:15 +0200

    #8 product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/8 edit product

--- product.php
+++ product.php
@@ -9,11 +9,11 @@ $editProduct = (!isset($_GET['editProduct']) ? 0 : $_GET['editProduct']);
 $stm = $connection->prepare('SELECT * FROM products WHERE id= ?');
 $stm->execute([$editProduct]);
 $product = $stm->fetch(PDO::FETCH_OBJ);
-
 $inputData = [
     'title' => (isset($product->title) && isset($editProduct)) ? $product->title : '',
     'description' => (isset($product->description) && isset($editProduct)) ? $product->description : '',
     'price' => (isset($product->price) && isset($editProduct)) ? $product->price : '',
+    'imageName' => (isset($product->image) && isset($editProduct)) ? imagePath($product->image) : '',
 ];
 
 $inputErrors = [];
@@ -22,59 +22,73 @@ if (isset($_POST['save'])) {
     if (isset($_POST['title']) && $_POST['title']) {
         $inputData['title'] = $_POST['title'];
     } else {
-        $inputErrors['titleError'] = translate('Please enter a title product.');
+        $inputErrors['titleError'] = translate('Please enter a product title.');
     }
     if (isset($_POST['description']) && $_POST['description']) {
         $inputData['description'] = $_POST['description'];
     } else {
-        $inputErrors['descriptionError'] = translate('Please enter a description product.');
+        $inputErrors['descriptionError'] = translate('Please enter a product description.');
     }
     if (isset($_POST['price']) && $_POST['price']) {
         $inputData['price'] = $_POST['price'];
-    } else {
-        $inputErrors['priceError'] = translate('Please enter a price.');
+        if (!is_numeric($_POST['price']) || intval($_POST['PRICE'])) {
+            $inputErrors['priceError'] = translate('Please enter a natural number for product price.');
+        }
     }
 
     if (isset($_FILES['image']) && $_FILES['image']['tmp_name']) {
         $inputData['imageLocation'] = $_FILES['image']['tmp_name'];
         $inputData['imageName'] = $_FILES['image']['name'];
     } else {
-        $inputErrors['imageNameError'] = translate('Please choose an image.');
+        if (!isset($product->image)) {
+            $inputErrors['imageNameError'] = translate('Please choose an image.');
+        }
     }
 
     if (!count($inputErrors)) {
         if (!$editProduct) {
-            $image_path = 'uploads/' . time() . "." . $inputData['imageName'];
+            $pathImage = 'uploads/' . time() . $inputData['imageName'];
             $sql = $connection->prepare('INSERT INTO products (title, description, price, image) VALUES( ?, ?, ?, ?)');
-            $sql->execute([$inputData['title'], $inputData['description'], $inputData['price'], $image_path]);
-            move_uploaded_file($inputData['imageLocation'], $image_path);
+            $sql->execute([$inputData['title'], $inputData['description'], $inputData['price'], $pathImage]);
+            move_uploaded_file($inputData['imageLocation'], $pathImage);
             header('Location: products.php');
             die();
         } else {
-            if (isset($product->image) && ($inputData['imageName'] == pathImage($product->image))) {
-                $sql = 'UPDATE products SET title= ? description= ? price= ? WHERE id= '.$editProduct;
+            if (isset($product->image) && $inputData['imageName'] == imagePath($product->image)) {
+                $sql = 'UPDATE products SET title= ?, description= ?, price= ? WHERE id= ?';
                 $params = [
                     $inputData['title'],
                     $inputData['description'],
                     $inputData['price'],
+                    $editProduct,
                 ];
-
             } else {
-                $sql = 'UPDATE products SET title= ? description= ? price= ? image= ? WHERE id= '.$editProduct;
-                $image_path = 'uploads/' . time() . "." . $inputData['imageName'];
+                $sql = 'UPDATE products SET title= ?, description= ?, price= ?, image= ? WHERE id= ?';
+                $pathImage = 'uploads/' . time() . $inputData['imageName'];
                 $params = [
                     $inputData['title'],
                     $inputData['description'],
                     $inputData['price'],
-                    $image_path,
+                    $pathImage,
+                    $editProduct,
                 ];
             }
             $updateProduct = $connection->prepare($sql);
             $updateProduct->execute($params);
+
+            $pathImage = 'uploads/' . time() . $inputData['imageName'];
+
+            if (!(isset($product->image) && $inputData['imageName'] == imagePath($product->image))) {
+                move_uploaded_file($inputData['imageLocation'], $pathImage);
+                if (isset($product->image) && file_exists($product->image)) {
+                    unlink($product->image);
+                }
+            }
+            header('Location: products.php');
+            die();
         }
     }
 }
-
 ?>
 
 <html>
@@ -105,9 +119,6 @@ if (isset($_POST['save'])) {
         <?= isset($inputErrors['priceError']) ? $inputErrors['priceError'] : '' ?>
     </span>
     <br><br>
-    <label for="image" >
-        <?=isset($product->image) ? $product->image : translate('Choose an image!') ?>
-    </label>
     <input type="file" id="image" name="image" placeholder="<?= translate('Image') ?>">
     <span>
         <?= isset($inputErrors['imageNameError']) ? $inputErrors['imageNameError'] : '' ?>
