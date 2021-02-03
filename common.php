6d0e91f85a0625f54800cadb6383f9569e32464d
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-02-03 15:00:58 +0200

    #3 Standards and procedures http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/3 code style

--- common.php
+++ common.php
@@ -9,9 +9,8 @@ function getDbConnection()
         $connection = new PDO('mysql:host=' . SERVER_NAME . ';dbname=' . DB_NAME . '', USERNAME, PASSWORD);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $connection;
-
     } catch (PDOException $exception) {
-        echo 'Connection failed: ' . $exception->getMessage();
+       return $exception->getMessage();
     }
 }
 
