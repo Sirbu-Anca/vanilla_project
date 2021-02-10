b66290db6c02391124b750d4981527972ff91041
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-02-10 14:29:53 +0200

    #8 product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/8

--- product.php
+++ product.php
@@ -4,12 +4,20 @@ require_once 'common.php';
 checkForAuthentication();
 $connection = getDbConnection();
 
-$editProductId = null;
-if (isset($_GET['editProductId'])) {
-    $editProductId = $_GET['editProductId'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $editProductId = $_GET['editProductId'] ?? null;
+} else {
+    $editProductId = $_POST['editProductId'] ?? null;
 }
 
-$product = new stdClass();
+$product = [
+    'id' => '',
+    'title' => '',
+    'description' => '',
+    'price' => '',
+    'image' => '',
+];
+
 if (!empty($editProductId)) {
     if (!is_numeric($editProductId)) {
         header('Location: products.php');
@@ -17,23 +25,16 @@ if (!empty($editProductId)) {
     }
     $stm = $connection->prepare('SELECT * FROM products WHERE id= ?');
     $stm->execute([$editProductId]);
-    $product = $stm->fetch(PDO::FETCH_OBJ);
+    $product = $stm->fetch();
     if (!$product) {
         header('Location: products.php');
         die();
     }
 }
 
-$inputData = [
-    'title' => isset($product->title) ? $product->title : '',
-    'description' => isset($product->description) ? $product->description : '',
-    'price' => isset($product->price) ? $product->price : '',
-    'imageName' => isset($product->image) ? imagePath($product->image) : '',
-];
-
-unset($_SESSION['oldInputs']);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     unset($_SESSION['inputErrors']);
+    unset($_SESSION['oldInputs']);
     $_SESSION['oldInputs'] = $_POST;
 
     if (!isset($_POST['title']) || !$_POST['title']) {
@@ -48,103 +49,107 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['inputErrors']['priceError'] = translate('Please enter a natural number for product price.');
         }
     } else {
-        $_SESSION['inputErrors']['priceError'] = translate('Please enter a number for product price.');
+        $_SESSION['inputErrors']['priceError'] = translate('Please enter a natural number for product price.');
     }
 
-    if (isset($_FILES['image']) && $_FILES['image']['tmp_name']) {
-        $inputData['imageLocation'] = $_FILES['image']['tmp_name'];
-        $inputData['imageName'] = $_FILES['image']['name'];
-    } else {
-        if (!isset($product->image)) {
-            $_SESSION['inputErrors']['imageNameError'] = translate('Please choose an image.');
+    if (!$editProductId && (!isset($_FILES['image']['tmp_name']) || !$_FILES['image']['tmp_name'])) {
+        $_SESSION['inputErrors']['imageNameError'] = translate('Please choose an image.');
+    }
+
+    if (isset($_SESSION['inputErrors']) && count($_SESSION['inputErrors'])) {
+        $location = 'Location: product.php';
+        if ($editProductId) {
+            $location .= '?editProductId=' . $editProductId;
         }
+        header($location);
+        die();
     }
 
-    $pathImage = 'uploads/' . time() . $inputData['imageName'];
-    if (empty($_SESSION['inputErrors'])) {
-        if (!$editProductId) {
-            $sql = $connection->prepare(
-                'INSERT INTO products (title, description, price, image) VALUES( ?, ?, ?, ?)');
-            $sql->execute([$_POST['title'], $_POST['description'], $_POST['price'], $pathImage]);
-            move_uploaded_file($inputData['imageLocation'], $pathImage);
-            header('Location: products.php');
-            die();
+    $pathImage = 'uploads/' . time() . $_FILES['image']['name'];
+    if ($editProductId) {
+        if (empty($_FILES['image']['name'])) {
+            $sql = 'UPDATE products SET title= ?, description= ?, price= ? WHERE id= ?';
+            $parameters = [
+                $_POST['title'],
+                $_POST['description'],
+                $_POST['price'],
+                $editProductId,
+            ];
         } else {
-            if (isset($product->image) && $inputData['imageName'] === imagePath($product->image)) {
-                $sql = 'UPDATE products SET title= ?, description= ?, price= ? WHERE id= ?';
-                $parameters = [
-                    $_POST['title'],
-                    $_POST['description'],
-                    $_POST['price'],
-                    $editProductId,
-                ];
-            } else {
-                $sql = 'UPDATE products SET title= ?, description= ?, price= ?, image= ? WHERE id= ?';
-                $parameters = [
-                    $_POST['title'],
-                    $_POST['description'],
-                    $_POST['price'],
-                    $pathImage,
-                    $editProductId,
-                ];
+            move_uploaded_file($_FILES['image']['tmp_name'], $pathImage);
+            $sql = 'UPDATE products SET title= ?, description= ?, price= ?, image= ? WHERE id= ?';
+            $parameters = [
+                $_POST['title'],
+                $_POST['description'],
+                $_POST['price'],
+                $pathImage,
+                $editProductId,
+            ];
+            if (isset($product['image']) && file_exists($product['image'])) {
+                unlink($product['image']);
             }
-            $updateProduct = $connection->prepare($sql);
-            $updateProduct->execute($parameters);
-
-            if (!(isset($product->image) && $inputData['imageName'] === imagePath($product->image))) {
-                move_uploaded_file($inputData['imageLocation'], $pathImage);
-                if (isset($product->image) && file_exists($product->image)) {
-                    unlink($product->image);
-                }
-            }
-            header('Location: products.php');
-            die();
         }
+        $updateProduct = $connection->prepare($sql);
+        $updateProduct->execute($parameters);
+        header('Location: products.php');
+        die();
+
+    } else {
+        move_uploaded_file($_FILES['image']['tmp_name'], $pathImage);
+        $sql = $connection->prepare(
+            'INSERT INTO products (title, description, price, image) VALUES( ?, ?, ?, ?)');
+        $sql->execute([$_POST['title'], $_POST['description'], $_POST['price'], $pathImage]);
+        header('Location: products.php');
+        die();
     }
 }
 ?>
 
-<html lang="">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= translate('Add product') ?></title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<h3><?= translate('Add your product details') ?></h3>
-<form action="" method="post" enctype="multipart/form-data">
-    <input type="text" name="title" placeholder="<?= translate('Title') ?>"
-           value="<?= isset($_SESSION['oldInputs']['title']) ? $_SESSION['oldInputs']['title'] : $inputData['title'] ?>">
-    <span class="error">
-        <?= isset($_SESSION['inputErrors']['titleError']) ? $_SESSION['inputErrors']['titleError'] : ''; ?>
+    <html lang="">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title><?= translate('Add product') ?></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <h3><?= translate('Add your product details') ?></h3>
+    <form action="product.php" method="post" enctype="multipart/form-data">
+        <?php if ($editProductId) : ?>
+            <input type="hidden" name="editProductId" value="<?= $product['id'] ?>">
+        <?php endif; ?>
+        <input type="text" name="title" placeholder="<?= translate('Title') ?>"
+               value="<?= isset($oldInputs['title']) ? $oldInputs['title'] : $product['title'] ?>">
+        <span class="error">
+        <?= isset($inputErrors['titleError']) ? $inputErrors['titleError'] : ''; ?>
     </span>
-    <br><br>
-    <input type="text" name="description" placeholder="<?= translate('Description') ?>"
-           value="<?= isset($_SESSION['oldInputs']['description']) ? $_SESSION['oldInputs']['description'] : $inputData['description'] ?>">
-    <span class="error">
-        <?= isset($_SESSION['inputErrors']['descriptionError']) ? $_SESSION['inputErrors']['descriptionError'] : ''; ?>
+        <br><br>
+        <input type="text" name="description" placeholder="<?= translate('Description') ?>"
+               value="<?= isset($oldInputs['description']) ? $oldInputs['description'] : $product['description'] ?>">
+        <span class="error">
+        <?= isset($inputErrors['descriptionError']) ? $inputErrors['descriptionError'] : ''; ?>
     </span>
-    <br><br>
-    <input type="number" name="price" placeholder="<?= translate('Price') ?> " min="0" step="any"
-           value="<?= isset($_SESSION['oldInputs']['price']) ? $_SESSION['oldInputs']['price'] : $inputData['price'] ?>">
-    <span class="error">
-        <?= isset($_SESSION['inputErrors']['priceError']) ? $_SESSION['inputErrors']['priceError'] : '' ?>
+        <br><br>
+        <input type="number" name="price" placeholder="<?= translate('Price') ?> " min="0" step="any"
+               value="<?= isset($oldInputs['price']) ? $oldInputs['price'] : $product['price'] ?>">
+        <span class="error">
+        <?= isset($inputErrors['priceError']) ? $inputErrors['priceError'] : '' ?>
     </span>
-    <br><br>
-    <input type="file" name="image" placeholder="<?= translate('Image') ?>">
-    <span class="error">
-        <?= isset($_SESSION['inputErrors']['imageNameError']) ? $_SESSION['inputErrors']['imageNameError'] : '' ?>
+        <br><br>
+        <input type="file" id="image" name="image" placeholder="<?= translate('Image') ?>">
+        <span class="error">
+        <?= isset($inputErrors['imageNameError']) ? $inputErrors['imageNameError'] : '' ?>
     </span>
-    <br><br>
-    <a href="products.php"><?= translate('Products') ?></a>
-    &nbsp;
-    <input type="submit" name="save" value="<?= translate('Save') ?>">
-</form>
+        <br><br>
+        <a href="products.php"><?= translate('Products') ?></a>
+        &nbsp;
+        <input type="submit" name="save" value="<?= translate('Save') ?>">
+    </form>
 
-</body>
-</html>
-
-
+    </body>
+    </html>
+<?php
+unset($_SESSION['inputErrors']);
+unset($_SESSION['oldInputs']);
