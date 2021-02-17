0664933c65e46c65f5ef7fef3688bafaa2cd85ff
Sirbu-Anca <ana.sirbu@5psolutions.eu>
2021-02-17 13:09:33 +0200

    #28 test http://pm.5psolutions.net/public/index.php?path_info=projects/training-ana-sirbu/tasks/28 Display reviews and delete

---
+++ reviews.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once 'common.php';
+checkForAuthentication();
+$connection = getDbConnection();
+
+if (isset($_POST['deleteReview'])) {
+    $reviewToBeDeleted = $_POST['deleteReview'];
+    $sql = $connection->prepare('SELECT comment FROM reviews WHERE id= ?');
+    $sql->execute([$reviewToBeDeleted]);
+    $review = $sql->fetch(PDO::FETCH_OBJ);
+
+    $sql = $connection->prepare('DELETE FROM reviews WHERE id= ?');
+    $sql->execute([$reviewToBeDeleted]);
+
+    header('Location: reviews.php');
+    die();
+}
+
+$sql = $connection->prepare('SELECT r.id, r.comment, r.rating, p.title 
+    FROM reviews r 
+        INNER JOIN products p ON r.product_id = p.id
+    ORDER BY rating');
+$sql->execute();
+$reviews = $sql->fetchAll(PDO::FETCH_OBJ);
+
+if (!count($reviews)) {
+    die();
+}
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= translate('Reviews')?></title>
+</head>
+<body>
+<table>
+    <tr>
+        <th><?= translate('Id') ?></th>
+        <th><?= translate('Product') ?></th>
+        <th><?= translate('Comment') ?></th>
+        <th><?= translate('Rating') ?></th>
+        <th><?= translate('Action') ?></th>
+    </tr>
+    <?php foreach ($reviews as $review) : ?>
+        <tr>
+            <td>
+                <?= $review->id ?>
+            </td>
+            <td>
+                <?= $review->title ?>
+            </td>
+            <td>
+                <?= $review->comment ?>
+            </td>
+            <td>
+                <?= $review->rating ?>
+            </td>
+            <td>
+                <form action="reviews.php" method="post">
+                    <input type="hidden" name="deleteReview" value="<?= $review->id ?>">
+                    <button type="submit" name="delete"><?= translate('Delete') ?></button>
+                </form>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
